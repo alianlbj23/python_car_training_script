644a8c110872e4c08bcf395c35193b1748b35107
lintin528 <mickey2000528@gmail.com>
2024-01-11 16:58:03 +0800

    [FEATURE] add config to store global variables

--- training_refactor.py
+++ training_refactor.py
@@ -15,7 +15,7 @@ from UnityAdaptor import UnityAdaptor
 # from Entity import State
 import Entity
 import random
-
+from config import *
 import time
 
 import threading
@@ -27,45 +27,7 @@ from std_msgs.msg import String
 import base64
 from typing import Tuple
 
-DEG2RAD = 0.01745329251
-# ----------------environment--------------- #
-# users can change: max_times_in_episode, max_times_in_game, end_distance
-max_times_in_episode=30
-max_times_in_game=210
-# (reach goal, distance out)
-end_distance=(0.2, 7)
-stop_target=False
-target_fixed_sec=12
-# ----------------environment--------------- #
 
-# ----------- paths ------------------#
-# users should change the path to match yourself's one
-load_path = './Model/DDPG/0809_car/model'
-save_path = './Model/DDPG/0809_car/model'
-plot_path = './Model/DDPG/0809_car'
-log_path = './Model/DDPG/0809_car/log'
-# ------------paths -------------------#
-
-# -------------------------Agent -------------------#
-q_lr=0.001
-pi_lr=0.001
-gamma=0.99
-rho=0.005
-pretrained=False
-new_input_dims=17
-input_dims=13
-n_actions=2
-batch_size=100
-layer1_size=400
-layer2_size=300
-# ------------------------ Agent -------------------#
-
-# --------------UnityAdaptor -------------#
-action_range=600
-steering_angle_range=20
-# --------------UnityAdaptor -------------#
-epoch = 5000
-unityState = ''
 class AiNode(Node):
     def __init__(self):
         super().__init__("aiNode")
@@ -159,7 +121,7 @@ def initialize_state():
     return car_state_training
 def initialize_training_environment():
     msg_from_Unity = None
-    load_step = 0
+    load_step = load_step
     prev_pos = [0, 0]
     trail_original_pos = [0, 0]
     action_Unity_Unity_adaptor = [0, 0]
